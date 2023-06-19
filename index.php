f37eddf17e82a5d8e3c44709fb11f9bc9357ba3e
quangdangcodegym <quang.dang@codegym.vn>
2023-06-19 20:11:53 +0700

    moi code xong file index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Hello PHP0623";
+?>
+<p>Thẻ đoạn văn bản: paragraph is an easy to install Apache distribution
+     containing MariaDB, PHP, and Perl. Just download and start the installer. It's that easy.</p>
+
+<h1>Heading h1</h1>
+<h2>Heading h2</h2>
+<h5>Heading h5</h5>
+<h6>Heading h6</h6>
+
+<a name="abc" id="idThe" href=""></a>
+
+
+
